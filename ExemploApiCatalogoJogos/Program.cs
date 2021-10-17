9bc3ad8480f47249d4cf5de506f9bd533e91776a
Fabrício Steinle Amoroso <68117406+FabricioAmoroso@users.noreply.github.com>
2021-10-17 20:45:18 -0300

    DIO_ApiCatálogoJogos commit
    Projeto de uma API para catálogo de jogos replicando os passos feitos por Thiago Oliveira no bootcamp Decola Tech da Digital Innovation One

---
+++ ExemploApiCatalogoJogos/Program.cs
@@ -0,0 +1,29 @@
+//Projeto realizado replicando os passos feitos por Thiago Oliveira no bootcamp Decola Tech da Digital Innovation One
+//Uma API para catálogo de jogos
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploApiCatalogoJogos
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
